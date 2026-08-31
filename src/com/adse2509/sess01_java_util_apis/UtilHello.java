6892b33136067171b37b004f74f981d58b0434c9
zahraabdiweli <zahraabdhy0@gmail.com>
2026-08-31 22:46:57 +0300

    Add files via upload

---
+++ src/com/adse2509/sess01_java_util_apis/UtilHello.java
@@ -0,0 +1,16 @@
+package com.adse2509.sess01_java_util_apis;
+
+/**
+ *
+ * @author DELL
+ */
+public class UtilHello {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
